8c3bb0bc226a89a178d3926cf2a849450aa9cc52
keun66080823-ui <keun66080823@gmail.com>
2025-11-28 10:28:23 +0900

    thmyeleaf문법

--- demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
+++ demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kh.demo.model.dto.Student;
@@ -67,8 +68,17 @@ public class ExampleController {
 		
 		// src/main/resources/templates/example/ex1.html 로 forward
 		return "example/ex1";
+	
 	}
 	
+	@PostMapping("ex2") //	/example/ex2 POST 방식 요청 매핑
+	public String ex2(Model model) {
+		
+		model.addAttribute("str", "<h1> 테스트 중 &times; </h1>"); // request scope
+		
+		// src/main/resources/templates/example/ex2/html 로 forward
+		return "example/ex2";
+	}
 	
 	
 	
